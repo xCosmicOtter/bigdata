1c1de69cb5f690f83fa7ea81ac13f7feee2bd621
Alex <76488431+TopAgrume@users.noreply.github.com>
2024-03-26 17:17:25 +0100

    Alex analyzer (#2)
    * update(launch_project.sh): background docker compose with unique usage, replaced by a new build on bash file call
    
    * update: .gitignore to ignore boursorama data, analyzer.py to load data from de boursorama dataset in docker/data, launch_project to update the docker image each time that this is needed
    
    * feat(analyzer.py): function to add the data inside sqldb with timestamp as indexes

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -1,24 +1,50 @@
 import pandas as pd
 import numpy as np
-import sklearn
+import time
+from glob import glob
+from datetime import datetime
+# TODO: import logging
 
 import timescaledb_model as tsdb
 
 db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'db', 'monmdp')        # inside docker
 #db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'localhost', 'monmdp') # outside docker
 
-def store_file(name, website):
-    if db.is_file_done(name):
-        return
-    if website.lower() == "boursorama":
-        try:
-            df = pd.read_pickle("bourse/data/boursorama/" + name)  # is this dir ok for you ?
-        except:
-            year = name.split()[1].split("-")[0]
-            df = pd.read_pickle("bourse/data/boursorama/" + year + "/" + name)
-        # to be finished
+def parse_filename(filename):
+    timestamp_str = filename[21:-4].split()[1:3]
+    timestamp_str = ' '.join(timestamp_str)
+    return datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S.%f")
 
+def store_file(files, website):
+    if website.lower() != "boursorama":
+        return
+    #  if not db.is_file_done(filename) # Desactivated cz useless for now
+
+    data = {parse_filename(filename): pd.read_pickle(filename) for filename in files}
+    df = pd.concat(data).reset_index(level=1, drop=True)
+    print(df.head(100))
+
+    # db.df_write(df, 'test1') # To write inside database
+    # Write the current file as "stored in the database"
+    # We should probably use the "db" instance
 
 if __name__ == '__main__':
-    # store_file("compA 2020-01-01 09:02:02.532411", "boursorama")
-    print("Done")
+    available_years = [2019] #np.arange(2019, 2024)
+    markets = ["compA"] #np.array(["amsterdam", "compA", "compB", "peapme"])
+    count = 0
+
+    print(f"Work in progress...")
+    for year in available_years:
+        for market in markets:
+            starting_time = time.time()
+            path_pattern = f"data/boursorama/{year}/{market}*"
+
+            available_files = np.array(glob(path_pattern))
+            for i in range(0, len(available_files), 1000):
+                print(f"Batch: {i//1000} of 1K elements")
+                store_file(available_files[i:i+1000], "boursorama")
+
+            count += len(available_files)
+            print(f"{year}-{market} done on {len(available_files)} files in {time.time() - starting_time} seconds.")
+
+    print(f"Work done on {count} files, in {time.time() - starting_time} seconds.")
--- launch_project.sh
+++ launch_project.sh
@@ -1,4 +1,20 @@
-make docker/analyzer
-make docker/dashboard
+# Delete the docker compose images
 cd docker
+docker compose down
+echo "Shutting down docker compose images. Done."
+
+# Make analyzer again
+echo -n "Make: analyzer. "
+cd analyzer
+make > /dev/null 2>&1
+echo "Done."
+
+# Make dashboard again
+echo -n "Make: dashboard. "
+cd ../dashboard
+make > /dev/null 2>&1
+echo "Done."
+
+# Docker compose up
+cd ..
 docker compose up
